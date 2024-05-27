6744f10e45576bac39fef8bf5b1998dd6e1e42bd
Juliano <juliano.fernandes@paymenu.com.br>
2024-05-27 11:47:25 -0300

    Add files via upload

---
+++ global.d.ts
@@ -0,0 +1,21 @@
+﻿import { DataPoolService } from './app/services/data-pool-service'
+import { SupportServices } from "./app/services/support-services";
+
+export { }
+
+declare global {
+    var DataPool: DataPoolService = new DataPoolService(); //Inicializado dentro de AuthContext.js
+    var Support: SupportServices = new SupportServices(); //Inicializado dentro de AuthContext.js
+
+
+
+    interface Number {
+        FormatSymbolValue(SymbolName: string): string;
+        ToCurrencyFormat(): string;
+    }
+
+    interface Array {
+        IsNotNullOrEmpty(): boolean;
+    }
+}
+
